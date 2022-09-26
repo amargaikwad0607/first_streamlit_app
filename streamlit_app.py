c132b844404b5f4fb0cafc2043410486fd01c40b
Amar Gaikwad <114434401+amargaikwad0607@users.noreply.github.com>
2022-09-27 00:33:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('My Parent New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & bluberry oatmeal')
+streamlit.text('kael, spinach & rocket smoothie')
