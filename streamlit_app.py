89497ac95caef2b1374d1e9ed67dc70bdbbf3c79
Amar Gaikwad <114434401+amargaikwad0607@users.noreply.github.com>
2022-11-07 19:13:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,6 @@ streamlit.text(' 🐔Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,11 +24,14 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+#Write the data to the screen
+streamlit.text(fruityvice_response.json())# Deleted after implementation
 
-# write your own comment -what does the next line do? 
+
+# write your own comment -what does the next line do?- Normalization
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# write your own comment - what does this do?--Data in table format
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
